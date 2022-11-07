6375b780ec53fc6f55d9573be0b7a1046fcea858
KuzaFkto <kuzafkto@gmail.com>
2022-11-07 20:27:23 +0100

    changes

--- Ejercicio9.java
+++ Ejercicio9.java
@@ -18,9 +18,9 @@ public class Ejercicio9{
         float raiz =(b*b- 4*a*c);
         if (raiz>=0 && 2*a != 0){
             String ecuación = a + "x^2 +"+ b + "x + " +c;
-            System.out.printf("Las soluciones a la ecuación %s son:", ecuación );
-            System.out.printf("x = %.2f \n" + ((double)(-b)-(Math.sqrt(raiz))/(double)2*a));
-            System.out.printf("x = %.2f \n" + ((double)(-b)+(Math.sqrt(raiz))/(double)2*a));
+            System.out.printf("Las soluciones a la ecuación %s son: \n", ecuación );
+            System.out.printf("x = %.2f \n" , ((double)(-b)-(Math.sqrt(raiz))/(double)2*a));
+            System.out.printf("x = %.2f \n" , ((double)(-b)+(Math.sqrt(raiz))/(double)2*a));
 
         }else{
             System.out.println("La ecuación no tiene solución");
